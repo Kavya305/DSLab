34a3ad6abb808ab314d1e142134ed751e635cbf8
Kavya Singal <kavya.singal30@gmail.com>
2025-08-11 23:06:46 +0530

    Update Ques2.cpp

--- Assignment1/Ques2.cpp
+++ Assignment1/Ques2.cpp
@@ -12,7 +12,6 @@ int main() {
         cin >> arr[i];
     }
 
-    // Removing duplicates
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             if (arr[i] == arr[j]) {
