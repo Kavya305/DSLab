74a9fc8cee8f07f406df8407453be1609b8da20b
Kavya Singal <kavya.singal30@gmail.com>
2025-08-11 22:52:31 +0530

    Update Ques2.cpp

--- Assignment1/Ques2.cpp
+++ Assignment1/Ques2.cpp
@@ -16,12 +16,11 @@ int main() {
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             if (arr[i] == arr[j]) {
-                // Shift left to overwrite duplicate
                 for (int k = j; k < n - 1; k++) {
                     arr[k] = arr[k + 1];
                 }
-                n--;    // Reduce size
-                j--;    // Stay at same index to check again
+                n--;    
+                j--;    
             }
         }
     }
