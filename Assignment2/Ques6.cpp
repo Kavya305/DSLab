b350f761e87ac138c19b8ed99108bae55c0d80c4
Kavya <kavya.singal30@gmail.com>
2025-09-16 09:36:24 +0530

    Update Ques6.cpp

---
+++ Assignment2/Ques6.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 100
+
+struct Element {
+    int row, col, val;
+};
+
+struct SparseMatrix {
+    int rows, cols, nonZero;
+    Element data[MAX];
+};
+
+void readMatrix(SparseMatrix &s) {
+    cout << "Enter rows, cols and number of non-zero elements: ";
+    cin >> s.rows >> s.cols >> s.nonZero;
+
+    cout << "Enter row, col and value of each element:\n";
+    for (int i = 0; i < s.nonZero; i++) {
+        cin >> s.data[i].row >> s.data[i].col >> s.data[i].val;
+    }
+}
+
+
+void printMatrix(const SparseMatrix &s) {
+    cout << "Row\tCol\tVal\n";
+    for (int i = 0; i < s.nonZero; i++) {
+        cout << s.data[i].row << "\t" << s.data[i].col << "\t" << s.data[i].val << "\n";
+    }
+}
+
+// (a) Transpose
+SparseMatrix transpose(const SparseMatrix &s) {
+    SparseMatrix t;
+    t.rows = s.cols;
+    t.cols = s.rows;
+    t.nonZero = s.nonZero;
+
+    for (int i = 0; i < s.nonZero; i++) {
+        t.data[i].row = s.data[i].col;
+        t.data[i].col = s.data[i].row;
+        t.data[i].val = s.data[i].val;
+    }
+    return t;
+}
+
+// (b) Addition
+SparseMatrix add(const SparseMatrix &a, const SparseMatrix &b) {
+    SparseMatrix sum;
+    if (a.rows != b.rows || a.cols != b.cols) {
+        cout << "Addition not possible!\n";
+        sum.nonZero = 0;
+        return sum;
+    }
+
+    sum.rows = a.rows;
+    sum.cols = a.cols;
+    sum.nonZero = 0;
+
+    int i = 0, j = 0;
+    while (i < a.nonZero && j < b.nonZero) {
+        if (a.data[i].row < b.data[j].row ||
+           (a.data[i].row == b.data[j].row && a.data[i].col < b.data[j].col)) {
+            sum.data[sum.nonZero++] = a.data[i++];
+        } else if (b.data[j].row < a.data[i].row ||
+                  (b.data[j].row == a.data[i].row && b.data[j].col < a.data[i].col)) {
+            sum.data[sum.nonZero++] = b.data[j++];
+        } else {
+            Element temp = a.data[i];
+            temp.val += b.data[j].val;
+            if (temp.val != 0)
+                sum.data[sum.nonZero++] = temp;
+            i++;
+            j++;
+        }
+    }
+
+    while (i < a.nonZero) sum.data[sum.nonZero++] = a.data[i++];
+    while (j < b.nonZero) sum.data[sum.nonZero++] = b.data[j++];
+
+    return sum;
+}
+
+//Multiplication
+SparseMatrix multiply(const SparseMatrix &a, const SparseMatrix &b) {
+    SparseMatrix product;
+    if (a.cols != b.rows) {
+        cout << "Multiplication not possible!\n";
+        product.nonZero = 0;
+        return product;
+    }
+
+    product.rows = a.rows;
+    product.cols = b.cols;
+    product.nonZero = 0;
+
+    for (int i = 0; i < a.nonZero; i++) {
+        for (int j = 0; j < b.nonZero; j++) {
+            if (a.data[i].col == b.data[j].row) {
+                int r = a.data[i].row;
+                int c = b.data[j].col;
+                int v = a.data[i].val * b.data[j].val;
+
+                bool found = false;
+                for (int k = 0; k < product.nonZero; k++) {
+                    if (product.data[k].row == r && product.data[k].col == c) {
+                        product.data[k].val += v;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    product.data[product.nonZero].row = r;
+                    product.data[product.nonZero].col = c;
+                    product.data[product.nonZero].val = v;
+                    product.nonZero++;
+                }
+            }
+        }
+    }
+
+    return product;
+}
+
+int main() {
+    SparseMatrix A, B;
+
+    cout << "Enter first matrix:\n";
+    readMatrix(A);
+    cout << "Enter second matrix:\n";
+    readMatrix(B);
+
+    cout << "\nMatrix A:\n"; printMatrix(A);
+    cout << "\nMatrix B:\n"; printMatrix(B);
+
+    cout << "\nTranspose of A:\n"; printMatrix(transpose(A));
+
+    cout << "\nAddition (A + B):\n"; printMatrix(add(A, B));
+
+    cout << "\nMultiplication (A * B):\n"; printMatrix(multiply(A, B));
+
+    return 0;
+}
